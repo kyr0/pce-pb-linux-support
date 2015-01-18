394fa6e250b0296ab8f70441aefad826c8bc26f7
kyr0 <aron.homberg@gmail.com>
2015-01-18 22:37:53 +0100

    Implemented better startup script, globally executable; version bump 0.1.1

--- bin/pcepbn.js
+++ bin/pcepbn.js
@@ -27,7 +27,4 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-var path = require('path');
-
-// Run
-require(path.resolve(__filename, '../../lib/pcepbn.js')).cli();
+require(__dirname + '/../lib/pcepbn').cli();
--- pcepbn.sh
+++ pcepbn.sh
@@ -1,13 +1,24 @@
-#!/bin/sh
+#!/bin/bash
+
+pushd . > /dev/null
+
+SCRIPT_PATH="${BASH_SOURCE[0]}";
+if ([ -h "${SCRIPT_PATH}" ]) then
+  while([ -h "${SCRIPT_PATH}" ]) do cd `dirname "$SCRIPT_PATH"`; SCRIPT_PATH=`readlink "${SCRIPT_PATH}"`; done
+fi
+cd `dirname ${SCRIPT_PATH}` > /dev/null
+SCRIPT_PATH=`pwd`;
+
+popd  > /dev/null
 
 # use nodejs executable if available
 if node -v >/dev/null 2>&1; then
-    node ./bin/pcepbn.js $1 $2 $3 $4 $5
+    node $SCRIPT_PATH/bin/pcepbn.js $1 $2 $3 $4 $5
     exit 0
 fi
 
 # use nodejs executable if available
 if nodejs -v >/dev/null 2>&1; then
-    nodejs ./bin/pcepbn.js $1 $2 $3 $4 $5
+    nodejs $SCRIPT_PATH/bin/pcepbn.js $1 $2 $3 $4 $5
     exit 0
 fi
